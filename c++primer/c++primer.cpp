cf49c69fb244b231e619f10ad769becd1db3511c
wanglin <wanglintju@outlook.com>
2019-04-29 16:06:15 +0800

    添加项目文件。

---
+++ c++primer/c++primer.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+using  std::string;
+
+int main()
+{
+	vector<int> v{1,2,3,4,5,6,7,8,9};
+	for (auto i : v)
+	{
+		i *= i;
+	}
+	for (auto i : v)
+	{
+		cout << i << " "<<endl;
+	}
+	cout << v[8];
+	return 0;
+}
